940a24b02880a717ad1d6c99a40a819f7b0d957f
Matheo <matheo.brise17@gmail.com>
2019-05-29 14:48:31 +0200

    p

---
+++ masterpom/model/src/main/java/model/DAOMap.java
@@ -0,0 +1,128 @@
+package model;
+
+public class DAOMap {
+	private EntityMatrix matrix;
+	private Rockford rockford;
+	private Exit exit;
+	private static long serialVersionUID;
+	private static Logger LOGGER;
+
+
+	public PlayField() {
+		
+	}
+	
+	private EntityMatrix getMatrix() {
+		
+	}
+	
+	public static Logger getLOGGER{
+		
+	}
+	
+	private void init() {
+		
+	}
+	
+	public void display() {
+		
+	}
+	
+	public int getRowsCount() {
+		
+	}
+	
+	public int getColumnsCount() {
+		
+	}
+	
+	private Entity getEntityBelow() {
+		
+	}
+	
+	private Entity getEntityAbove() {
+				
+	}
+	
+	private Entity getEntityOnRight() {
+	
+	}
+	
+	private Entity getEntityOnLeft() {
+		
+	}
+	
+	private Entity getEntityBelowRight() {
+		
+	}
+	
+	private Entity getEntityBelowLeft() {
+		
+	}
+	
+	private Entity getEntityAboveRight() {
+		
+	}
+	
+	private Entity getEntityAboveLeft() {
+		
+	}
+	
+	public Entity getContent() {
+		
+	}
+	
+	private list<AbstractMovableEntity> getDiamondsAndBoulders(){
+		
+	}
+	
+	public void addEntity() {
+		
+	}
+	
+	public void initPlayerPosition() {
+		
+	}
+	
+	public void initExitPosition() {
+		
+	}
+	public Rockford getRockford() {
+		
+	}
+	
+	public Exit getExit() {
+		
+	}
+	
+	public void moveRockfort(Direction) {
+		
+	}
+	
+	private void move(Entity, Direction) {
+		
+	}
+	
+	private boolean pushBoulder(Boulder, Direction) {
+		
+	}
+	
+	public void updateMovableEntities() {
+		
+	}
+	
+	private void fall(AbstactMovableEntities) {
+		
+	}
+	private void explose(AbstractMovableEntity) {
+		
+	}
+	private void deplace(AbstractMovableEntity, Direction) {
+		
+	}
+	public int hashCode() {
+		
+		
+	}
+
+}
---
+++ masterpom/model/src/main/java/model/Level.java
@@ -0,0 +1,59 @@
+package model;
+
+public class Level {
+	private int DiamondsToWin;
+	private boolean pause;
+	private String name;
+	private static long serialVersionUID;
+	private static Logger LOGGER;
+	
+	public Level(int rows, int Columns, int DiamondsToWin) {
+		
+	}
+	
+	public int getDiamondsToWin() {
+		
+	}
+	
+	public Stying getName() {
+		
+	}
+	
+	public void setName(Stying name) {
+		
+	}
+	
+	public PlayField getPlayField() {
+		
+	}
+	
+	public void start() {
+		
+	}
+	
+	public boolean inPause() {
+		
+	}
+	
+	public boolean isFinish() {
+		
+	}
+	
+	public void updateExitState() {
+		
+	}
+	
+	public void setPause(boolean) {
+		
+	}
+	
+	public void addElement(int x, int y, Entity entity) {
+		
+	}
+	public void initRockfordPosition(int x, int y) {
+		
+	}
+	public void initExitPosition(int x, int y) {
+		
+}
+}
